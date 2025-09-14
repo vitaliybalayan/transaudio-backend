c468b49992efffca79ac9f61a1975f5aa65b4f85
vitaliybalayan <vitaliy.balayan@gmail.com>
2025-09-15 02:22:35 +0500

    .env.example pull

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
-// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -11,15 +9,8 @@ async function bootstrap() {
 	const config = app.get(ConfigService);
 
 	app.use(
-		// config.getOrThrow<string>('GRAPHQL_PREFIX'),
 		graphqlUploadExpress({ maxFileSize: 50000000, maxFiles: 10 }),
 	);
-
-	// app.useGlobalPipes(
-	// 	new ValidationPipe({
-	// 		transform: true,
-	// 	}),
-	// );
 
 	app.enableCors({
 		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
